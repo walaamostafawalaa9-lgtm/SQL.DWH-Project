ca12f3daff02e8f509834d4fab21fa9fbafc3ea0
walaamostafawalaa9-lgtm <walaamostafawalaa9@gmail.com>
2026-08-19 22:19:22 +0300

    Create DataWarhouse database and schemas
    This script creates a new database named 'DataWarhouse' and sets up three schemas: 'bronze', 'silver', and 'gold'. It includes a warning about data loss if the database already exists.

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,1 +1,34 @@
 
+/*
+===========================================
+create database and it's schemas
+===========================================
+Script Purpose:
+ this script creates a new databse named 'DataWarhouse' after checking if it is alreay exists.
+ If the database exists ,it is dropped and recreated. Additionally the script sets up three schemas
+ within the database: 'bronze, 'silver', and 'gold'.
+
+ Warning:
+  Running this script will drop the entire database if it exists.
+  All data in the database will be permantly deleted. Proceed with caution 
+  and ensure that you have proper backups before running this script.
+*/
+use master;
+go
+----drop dwh if exist to recreate---
+if exists(select 1 from sys.databases where name='DataWarhouse')
+begin
+alter database DataWarhouse  set SINGLE_USER with Rollback IMMEDIATE ;
+drop database DataWarhouse 
+end;
+go
+----create dwh database---
+create database DataWarhouse;
+use DataWarhouse;
+Go
+-----create schemas-----
+ create schema bronze;
+  go
+  create schema silver;
+  Go
+   create schema gold;
